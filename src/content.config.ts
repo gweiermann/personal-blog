72e40b32dc9480fda6d5d86c4e0080096b34ca20
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2026-03-07 18:01:38 +0100

    add possibility to add thumbnail and other assets

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,14 +3,21 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    publishDate: z.coerce.date(),
-    category: z.string(),
-    readTimeMinutes: z.number().optional(),
+  loader: glob({
+    pattern: '**/*.{md,mdx}',
+    base: './src/content/blog',
+    generateId: ({ entry }) =>
+      entry.replace(/\/index\.(mdx|md)$/i, '').replace(/\.(mdx|md)$/i, ''),
   }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      publishDate: z.coerce.date(),
+      category: z.string(),
+      readTimeMinutes: z.number().optional(),
+      thumbnail: image().optional(),
+    }),
 });
 
 const projects = defineCollection({
